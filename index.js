8505bcc7c3d2066f1008349dbb8e3695e0f08afc
rimasultana <rimasultana20225@gmail.com>
2024-03-06 18:06:08 +0600

    continune

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+// let text = "";
+
+
+// for (let i = 0; i < 10; i++) {
+//   if (i === 3) { break; }
+//   text += "The number is " + i;
+// }
+
+// console.log(text);
+
+
+
 let text = "";
-
-
 for (let i = 0; i < 10; i++) {
-  if (i === 3) { break; }
+  if (i === 3) { continue; }
   text += "The number is " + i;
 }
 
